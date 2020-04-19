f9389475963a84c45c071114ad043512edd4fb37
s41m0n <simonemagnani.96@gmail.com>
2020-04-19 03:00:41 +0200

    Multi session tracking & restart (still memory leak due to missing LRU)

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # eBPF_TrafficAnalyzer
+
+Simple eBPF C program to analyze some packets' feature in order to detect (and possibly prevent) a possible ongoing network attack.
+
+## How does it work
+
+## Usage
+
+## Pointers
+
+<https://www.freeformatter.com/json-escape.html>
+<https://github.com/iovisor/bcc/blob/master/docs/reference_guide.md>
+<https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=29ba732acbeece1e34c68483d1ec1f3720fa1bb3>
+<https://support.cumulusnetworks.com/hc/en-us/articles/216509388-Throughput-Testing-and-Troubleshooting>
+<https://iris.polito.it/retrieve/handle/11583/2712562/207457/18HPSR-ebpf-lessons-learned.pdf>
--- feature_extractor.c
+++ feature_extractor.c
@@ -7,11 +7,46 @@
  *  - ICMP
  * It still lacks of decision making about which TCP session should be tracked (WIP).
  */
+/*Protocol types according to standard*/
 #define IPPROTO_TCP 6
 #define IPPROTO_UDP 17
 #define IPPROTO_ICMP 1
 
-#define N_PACKET 100
+/*Own control variables*/
+#define N_PACKET 10000
+#define N_SESSION 50
+
+/*Structure containing info about capture and valid indexes*/
+struct capture_info {
+    unsigned int feature_map_index;
+    unsigned int n_session_tracking;
+    uint64_t last_ins_tstamp;
+} __attribute__((packed));
+
+/*TCP-SESSION identifier*/
+struct tcp_session {
+    __be32  saddr;
+    __be32  daddr;
+    __be16  sport;
+    __be16  dport;
+} __attribute__((packed));
+
+/*Features to be exported*/
+struct features {
+    uint32_t saddr;
+    uint32_t daddr;
+    uint64_t timestamp;
+    uint16_t length;
+    uint16_t ipv4_flags;
+    uint16_t tcp_len;
+    uint32_t tcp_ack;
+    uint8_t  tcp_flags;
+    uint16_t tcp_win;
+    /*
+    uint8_t udp_len;
+    uint8_t  icmp_type;
+    */
+} __attribute__((packed));
 
 /*Ethernet Header*/
 struct eth_hdr {
@@ -106,30 +141,18 @@ struct icmphdr {
   } un;
 };
 
-/*Features to be exported*/
-struct features {
-	uint32_t saddr;
-	uint32_t daddr;
-    uint64_t timestamp;
-    uint16_t length;
-    uint16_t ipv4_flags;
-    uint16_t tcp_len;
-    uint32_t tcp_ack;
-    uint8_t  tcp_flags;
-    uint16_t tcp_win;
-    uint8_t udp_len;
-    uint8_t  icmp_type;
-} __attribute__((packed));
-
 /*Structures shared between Control Plane - Data Plane*/
-BPF_ARRAY(INDEX_COUNTER, unsigned int, 1);
-BPF_ARRAY(PACKET_INFO, struct features, N_PACKET);
+BPF_ARRAY(CAPTURE_INFO, struct capture_info, 1);
+BPF_ARRAY(PACKET_FEATURE_MAP, struct features, N_PACKET);
+
+/*Internal structure to check whether a session is tracked or not*/
+BPF_HASH(TCP_SESSIONS_TRACKED, struct tcp_session);
 
 static __always_inline int handle_rx(struct CTXTYPE *ctx, struct pkt_metadata *md) {
     void *data = (void *) (long) ctx->data;
     void *data_end = (void *) (long) ctx->data_end;
  	
-     /*Parsing L2*/
+    /*Parsing L2*/
     struct eth_hdr *ethernet = data;
     if (data + sizeof(*ethernet) > data_end)
         return RX_OK;
@@ -145,33 +168,39 @@ static __always_inline int handle_rx(struct CTXTYPE *ctx, struct pkt_metadata *m
         return RX_OK;
 
     /*Checking for considered protocols*/
- 	if(ip->protocol != IPPROTO_TCP && ip->protocol != IPPROTO_UDP && ip->protocol != IPPROTO_ICMP){
+    if(ip->protocol != IPPROTO_TCP && ip->protocol != IPPROTO_UDP && ip->protocol != IPPROTO_ICMP){
  		return RX_OK;
  	}
 
- 	/*Retrieving data structures*/
+ 	/*Retrieving capture information*/
  	unsigned int key = 0;
- 	unsigned int *index_counter = INDEX_COUNTER.lookup(&key);
- 	if (!index_counter || *index_counter >= N_PACKET){
+ 	struct capture_info *cinfo = CAPTURE_INFO.lookup(&key);
+ 	if (!cinfo){
     	return RX_OK;
  	}
- 	struct features *pkt_info =  PACKET_INFO.lookup(index_counter);
+
+    /*Checking if packed is already timestamped, otherwise get it from kernel bpf function*/
+    uint64_t curr_time = ctx->tstamp == 0? bpf_ktime_get_ns() : ctx->tstamp;
+
+    /*Checking if array of captured packets is full*/
+    if(cinfo->feature_map_index == N_PACKET) {
+        /*Checking if last insertion happened 10s ago*/
+        if(curr_time - cinfo->last_ins_tstamp < 10000000000) {
+            pcn_log(ctx, LOG_TRACE, "Number of packet to be stored reached!");
+            return RX_OK;
+        }
+        /*Reset head to zero and prepare to accept other sessions (NB: the old ones are not deleted)*/
+        /*TODO: dig deeper for BPF_MAP_TYPE_LRU_HASH*/
+        cinfo->feature_map_index = 0;
+        cinfo->n_session_tracking = 0;
+        pcn_log(ctx, LOG_TRACE, "Enough time passed, RESTARTING FROM INDEX 0!");
+    }
+
+    /*Retrieving current features slot*/
+ 	struct features *pkt_info =  PACKET_FEATURE_MAP.lookup(&cinfo->feature_map_index);
  	if (!pkt_info){
     	return RX_OK;
  	}
-
- 	/*Setting timestamp*/
- 	if (ctx->tstamp == 0) {
-		pkt_info->timestamp = bpf_ktime_get_ns();
-	} else {
-		pkt_info->timestamp = ctx->tstamp;
-	}
-
-	/*Setting Ip info*/
-	pkt_info->saddr = bpf_ntohl(ip->saddr);
-	pkt_info->daddr = bpf_ntohl(ip->daddr);
-	pkt_info->length = bpf_ntohs(ip->tot_len);
-	pkt_info->ipv4_flags = bpf_ntohs(ip->frag_off);
 
     switch(ip->protocol) {
     	case IPPROTO_TCP: {
@@ -180,35 +209,72 @@ static __always_inline int handle_rx(struct CTXTYPE *ctx, struct pkt_metadata *m
 		 	if(data + sizeof(struct eth_hdr) + sizeof(struct iphdr) + sizeof(*tcp) > data_end) {
  				return RX_OK;
             }
- 			pkt_info->tcp_ack = tcp->ack_seq;
+            /*Check if it is already tracked*/
+            struct tcp_session session = {.saddr=ip->saddr, .daddr=ip->daddr, .sport=tcp->source, .dport=tcp->dest};
+            uint64_t *is_tracked = TCP_SESSIONS_TRACKED.lookup(&session);
+            if(!is_tracked) {
+                /*Check if already tracking enough session*/
+                if(cinfo->n_session_tracking == N_SESSION) {
+                    pcn_log(ctx, LOG_TRACE, "Number of session tracked reached!");
+                    return RX_OK;
+                }
+                /*Increase tracked sessions and store current one in map*/
+                cinfo->n_session_tracking+=1;
+                uint64_t val = 1;
+                TCP_SESSIONS_TRACKED.insert(&session, &val);
+                pcn_log(ctx, LOG_TRACE, "New tracked session {%u, %u, %u, %u}", session.saddr, session.daddr, session.sport, session.dport);
+            } else {
+                pcn_log(ctx, LOG_TRACE, "Already tracking session {%u, %u, %u, %u}", session.saddr, session.daddr, session.sport, session.dport);
+            }
+            
+            /*Updating last inserted packet timestamp*/
+            cinfo->last_ins_tstamp = curr_time;
+
+            /*Setting packet features*/
+            pkt_info->tcp_ack = tcp->ack_seq;
  			pkt_info->tcp_win = bpf_ntohs(tcp->window);
  			pkt_info->tcp_len = (uint16_t)(pkt_info->length - sizeof(struct iphdr) - sizeof(*tcp));
  			pkt_info->tcp_flags = (tcp->cwr << 7) | (tcp->ece << 6) | (tcp->urg << 5) | (tcp->ack << 4)
  			 					| (tcp->psh << 3)| (tcp->rst << 2) | (tcp->syn << 1) | tcp->fin;
-            pcn_log(ctx, LOG_INFO, "Inserted TCP packet at index: %d ", *index_counter);
  			break;
     	}
+        default : {
+            return RX_OK;
+        }
+        /*
     	case IPPROTO_ICMP: {
-            /*Parsing L4 ICMP*/
+            //Parsing L4 ICMP
             struct icmphdr *icmp = data + sizeof(struct eth_hdr) + sizeof(struct iphdr);
             if(data + sizeof(struct eth_hdr) + sizeof(struct iphdr) + sizeof(*icmp) > data_end) {
                 return RX_OK;
             }
             pkt_info->icmp_type = icmp->type;
-            pcn_log(ctx, LOG_INFO, "Inserted ICMP packet at index: %d ", *index_counter);
+            pcn_log(ctx, LOG_INFO, "Inserted ICMP packet at index: %d ", *CAPTURE_INFO);
     		break;
         }
     	case IPPROTO_UDP: {
-            /*Parsing L4 UDP*/
+            //Parsing L4 UDP
             struct udphdr *udp = data + sizeof(struct eth_hdr) + sizeof(struct iphdr);
             if(data + sizeof(struct eth_hdr) + sizeof(struct iphdr) + sizeof(*udp) > data_end) {
                 return RX_OK;
             }
             pkt_info->udp_len = bpf_ntohs(udp->len);
-            pcn_log(ctx, LOG_INFO, "Inserted UDP packet at index: %d ", *index_counter);
+            pcn_log(ctx, LOG_INFO, "Inserted UDP packet at index: %d ", *CAPTURE_INFO);
     		break;
-        }
+        }*/
     }
-    *index_counter+=1;
+
+    /*Setting packet features*/
+    pkt_info->saddr = bpf_ntohl(ip->saddr);
+    pkt_info->daddr = bpf_ntohl(ip->daddr);
+    pkt_info->length = bpf_ntohs(ip->tot_len);
+    pkt_info->ipv4_flags = bpf_ntohs(ip->frag_off);
+    pkt_info->timestamp = curr_time;
+
+    pcn_log(ctx, LOG_TRACE, "Inserted Packet at index: %u ", cinfo->feature_map_index);
+
+    /*The capture was fine so increase the index*/
+    cinfo->feature_map_index+=1;
+
 	return RX_OK;
 }
