917baaa5935e30e0addf861a3ee6a78e50eee91d
Simone Magnani <simonemagnani.96@gmail.com>
2020-04-22 00:33:28 +0200

    Improved program with more featuresextracted and tracking all sessions (ICMP, UDP, TCP) + Added debug mode and json output to dynmon_extractor.c (#2)
    Improved program with more featuresextracted and tracking all sessions (ICMP, UDP, TCP) + Added debug mode and json output to dynmon_extractor.c

--- tools/dynmon_extractor.py
+++ tools/dynmon_extractor.py
@@ -14,9 +14,8 @@ POLYCUBED_ADDR = 'localhost'
 POLYCUBED_PORT = 9000
 REQUESTS_TIMEOUT = 5 #seconds
 OUTPUT_DIR = 'dump'
-FILE_FORMAT = 'csv'
 CAPTURE_INFO_MAP_NAME = "CAPTURE_INFO"
-PACKET_FEATURE_MAP_NAME = "PACKET_FEATURE_MAP"
+PACKET_FEATURE_MAP_NAME = "PACKET_BUFFER"
 
 polycubed_endpoint = 'http://{}:{}/polycube/v1'
 
@@ -29,7 +28,7 @@ def main():
 	port = args['port']
 	cube_name = args['cube_name']
 	output_dir = args['output']
-	file_format = args['file_format']
+	debug = args['debug']
 
 	capture_map_name = args['capture_map_name']
 	packet_feature_map_name = args['packet_feature_map_name']
@@ -53,34 +52,61 @@ def main():
 	for metric in metrics:
 		value = json.loads(metric['value'])
 		if metric['name'] == capture_map_name:
-			entry_index = int(value['feature_map_index']) - 1 if int(value['feature_map_index'])-1 >= 0 else 0 
+			entry_index = int(value['next_index']) - 1 if int(value['next_index'])-1 >= 0 else 0 
 			print('Information concerning the actual capture')
-			print('\tEntryValidIndex: [0-{}]'.format(value['feature_map_index']))
-			print('\tSessionTracked: {}'.format(value['n_session_tracking']))
+			print('\tEntryValidIndex: [0-{}]'.format(entry_index))
+			print('\tSessionsTracked: {}'.format(value['n_session_tracking']))
 		elif metric['name'] == packet_feature_map_name:
-			for packet in value[:entry_index]:
-				timestamp = packet['timestamp']
-				seconds = timestamp // 1000000000
-				nanoseconds = str(timestamp)[:9]
-				saddr = socket.inet_ntoa(struct.pack('!L', packet['saddr']))
-				daddr = socket.inet_ntoa(struct.pack('!L', packet['daddr']))
-				file = open(f"{output_dir}/{saddr}-{packet['sport']}___{daddr}-{packet['dport']}___{timestamp}.{file_format}", 'w')
-				file.write(f"Seconds     ,\t{seconds}\n")
-				file.write(f"Ns          ,\t{nanoseconds}\n")
-				file.write(f"Length      ,\t{packet['length']}\n")
-				file.write(f"IPv4 flags  ,\t{packet['ipv4_flags']}\n")
-				file.write(f"TCP len     ,\t{packet['tcp_len']}\n")
-				file.write(f"TCP ACK     ,\t{packet['tcp_ack']}\n")
-				file.write(f"TCP flags   ,\t{packet['tcp_flags']}\n")
-				file.write(f"TCP Win     ,\t{packet['tcp_win']}\n")
-				file.write(f"UDP len     ,\t{packet.get('udp_len', 0)}\n")
-				file.write(f"ICMP type   ,\t{packet.get('icmp_type', 0)}\n")
-				file.write
-				file.close()
+			valid_entries = value[:entry_index]
+			printDebug(valid_entries, output_dir) if debug is True else None
+			reassembleAndPrint(valid_entries, output_dir)
 		else:
 			#Add here for more metric to be parsed
 			print('Ignored metric')
 
+
+def reassembleAndPrint(packets, output_dir):
+	flows = {}
+	for packet in packets:
+		saddr = socket.inet_ntoa(struct.pack('!L', packet['srcIp']))
+		daddr = socket.inet_ntoa(struct.pack('!L', packet['dstIp']))
+		flowIdentifier = (saddr, packet['srcPort'], daddr, packet['dstPort'], packet['protocol'])
+		features = []
+		for key, value in packet.items():
+			print(key)
+			if key not in ['srcIp', 'srcPort', 'dstIp', 'dstPort', 'protocol']:
+				features.append(value)
+		if flowIdentifier in flows:
+			flows[flowIdentifier].append(features)
+		else:
+			flows[flowIdentifier] = [features]
+	data = {"flows": []}
+	for key, value in flows.items():
+		data['flows'].append({"id": key, "packets": value})
+	with open(f'{output_dir}/result.json', 'w') as fp:
+		json.dump(data, fp, indent=2)
+
+
+def printDebug(packets, output_dir):
+	for packet in packets:
+		timestamp = packet['timestamp']
+		seconds = timestamp // 1000000000
+		nanoseconds = str(timestamp)[:9]
+		saddr = socket.inet_ntoa(struct.pack('!L', packet['srcIp']))
+		daddr = socket.inet_ntoa(struct.pack('!L', packet['dstIp']))
+		file = open(f"{output_dir}/{saddr}-{packet['srcPort']}___{daddr}-{packet['dstPort']}___{timestamp}.csv", 'w')
+		file.write(f"Seconds     ,\t{seconds}\n")
+		file.write(f"Ns          ,\t{nanoseconds}\n")
+		file.write(f"Length      ,\t{packet['length']}\n")
+		file.write(f"IPv4 flags  ,\t{packet['ipFlagsFrag']}\n")
+		file.write(f"TCP len     ,\t{packet['tcpLen']}\n")
+		file.write(f"TCP ACK     ,\t{packet['tcpAck']}\n")
+		file.write(f"TCP flags   ,\t{packet['tcpFlags']}\n")
+		file.write(f"TCP Win     ,\t{packet['tcpWin']}\n")
+		file.write(f"UDP len     ,\t{packet['udpSize']}\n")
+		file.write(f"ICMP type   ,\t{packet['icmpType']}\n")
+		file.write
+		file.close()
 
 def checkIfServiceExists(cube_name):
 	try:
@@ -124,7 +150,7 @@ def parseArguments():
 	parser.add_argument('-a', '--address', help='set the polycube daemon ip address', type=str, default=POLYCUBED_ADDR)
 	parser.add_argument('-p', '--port', help='set the polycube daemon port', type=int, default=POLYCUBED_PORT)
 	parser.add_argument('-o', '--output', help='set the output directory', type=str, default=OUTPUT_DIR)
-	parser.add_argument('-f', '--file_format', help='set the output files format', type=str, default=FILE_FORMAT)
+	parser.add_argument('-d', '--debug', help='set the debug mode, to print also single packets file in the directory as .csv', action='store_true')
 	parser.add_argument('-cm', '--capture_map_name', help='set the capture map name (the same in the json file)', type=str, default=CAPTURE_INFO_MAP_NAME)
 	parser.add_argument('-pm', '--packet_feature_map_name', help='set the packet feature map name (the same in the json file)', type=str, default=PACKET_FEATURE_MAP_NAME)
 	return parser.parse_args().__dict__
