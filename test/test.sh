a381d17c22ea593e92eb8a02336a6b106a4af71e
Simone Magnani <simonemagnani.96@gmail.com>
2020-06-15 17:33:42 +0200

    Improved test.sh to work in every environment

--- test/test.sh
+++ test/test.sh
@@ -32,8 +32,11 @@ function delete_veth {
 
 function cleanup {
   	set +e
-  	polycubectl simplebridge del br1
-  	polycubectl monitor del
+    #polycubectl simplebridge del br1
+    curl -X DELETE http://localhost:9000/polycube/v1/simplebridge/br1
+  	#polycubectl monitor del
+    curl -X DELETE http://localhost:9000/polycube/v1/dynmon/monitor
+
   	delete_veth 2
     sudo pkill iperf3
 }
@@ -58,14 +61,18 @@ function main {
 
   create_veth 2
 
-  polycubectl simplebridge add br1
+  #polycubectl simplebridge add br1
+  curl -d '' -H  "Content-Type: application/json" http://localhost:9000/polycube/v1/simplebridge/br1
 
-  polycubectl simplebridge br1 ports add port1 peer=veth1
-  polycubectl simplebridge br1 ports add port2 peer=veth2
+  #polycubectl simplebridge br1 ports add port1 peer=veth1
+  curl -d '{"peer": "veth1"}' -H "Content-Type: application/json" http://localhost:9000/polycube/v1/simplebridge/br1/ports/port1
 
-  sleep 1
+  #polycubectl simplebridge br1 ports add port2 peer=veth2
+  curl -d '{"peer": "veth2"}' -H "Content-Type: application/json" http://localhost:9000/polycube/v1/simplebridge/br1/ports/port2
+
+  sleep 2
   ../tools/dynmon_injector.py monitor br1:port1 $test_file
-  sleep 1
+  sleep 2
 
   sudo ip netns exec ns2 iperf3 -s &>/dev/null &
   sleep 1
